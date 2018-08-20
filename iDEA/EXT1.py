Fixed normalisation issue in EXT/NON
This doesn't affect any results, but the normalisation
being printed to the screen in the real time propagation
is wrong, and therefore misleading. The fix checks that
the wavefunction is correctly normalised at each time
step.

iDEA/EXT1.py
@@ -218,7 +218,8 @@ def main(parameters):
             # Normalise the wavefunction 
             norm = npla.norm(wavefunction)*np.sqrt(pm.space.delta)
             wavefunction /= norm
-            string = 'EXT: t = {:.5f}, normalisation = {}'.format(i*pm.sys.deltat, norm**2)
+            norm = npla.norm(wavefunction)*np.sqrt(pm.space.delta)
+            string = 'EXT: t = {:.5f}, normalisation = {}'.format(i*pm.sys.deltat, norm)
             pm.sprint(string, 1, newline=False)
 
             # Calculate the density